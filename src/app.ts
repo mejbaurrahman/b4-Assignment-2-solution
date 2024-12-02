534e7498c2555daaca250f7ffd85939f59038084
unknown <mejbacsedu@gmail.com>
2024-12-02 15:30:48 +0600

    upload

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { ProductRoutes } from './app/modules/product/product.route';
+import { OrderRoutes } from './app/modules/order/order.route';
+import { StatusCodes } from 'http-status-codes';
+
+const app = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/products', ProductRoutes);
+app.use('/api/orders', OrderRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.get('*', function (req, res) {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app from './app';
+
+import mongoose from 'mongoose';
+import config from './app/config';
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(config.database_url as string);
+
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`);
+  });
+}
